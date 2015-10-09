e7793ab98ac5faae05ad374cddcf97b8f3dbf049
Chris Mague <github@mague.com>
2015-10-09 09:42:30 -0700

    easier ip settings

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -68,7 +68,7 @@ resource "docker_container" "consul3" {
 ######################################################################################
 # Set an example kye in the key/value store
 provider "consul" {
-  address    = "127.0.0.1:8500"
+  address    = "${docker_container.consul-master.ip_address}:8500"
   datacenter = "docker"
   scheme     = "http"
 }
